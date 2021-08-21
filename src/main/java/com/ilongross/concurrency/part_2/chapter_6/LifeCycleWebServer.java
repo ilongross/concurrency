c8e6c47873b733c20d72a12fa8b8349d0c2a41c6
Ilya <71347432+ilongross@users.noreply.github.com>
2021-08-22 00:21:44 +0800

    1. add description in LifeCycleWebServer.java

--- src/main/java/com/ilongross/concurrency/part_2/chapter_6/LifeCycleWebServer.java
+++ src/main/java/com/ilongross/concurrency/part_2/chapter_6/LifeCycleWebServer.java
@@ -9,6 +9,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
 
+// Веб-сервер с поддержкой выключения
 
 public class LifeCycleWebServer {
 
